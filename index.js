b37ec43377e1e352c1f42bfda7b0abc39c2dce11
hannasukharieva <annsukhareva33@gmail.com>
2021-06-16 22:08:25 +0100

    Adds the ability to add X and 0 to cells

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ window.addEventListener('load', () => {
   app.append(wrapper);
   wrapper.classList.add('wrapper')
 
+  const cells = [];
+
   for (let i = 0; i < 9; i++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     wrapper.append(cell);
+    cells.push(cell)
   }
+
+  let step = 1;
+
+  cells.forEach(element => {
+    element.addEventListener('click', () => {
+      /* Brute force solution */
+      // if (element.innerText === '' && step % 2 !== 0 ) {
+      //   element.innerText = 'X';
+      //   step++;
+      // } else if (element.innerText === '') {
+      //   element.innerText = '0';
+      //   step++;
+      // }
+      
+      /* Refectored version */
+      if (element.innerText === '') {
+        element.innerText = step % 2 !== 0 ? 'X' : 'O';
+        step++;
+      }
+    })
+  });
+
+  console.log(cells)
+
 });
